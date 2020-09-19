eee97b632b04a090c65b6619ef6d39c49926c078
elsonss1988 <elsonss1988@gmail.com>
2020-09-19 15:55:32 -0300

    resetBoard

--- js/script.js
+++ js/script.js
@@ -1,7 +1,14 @@
 const cards =document.querySelectorAll(".card");
 var hasFlippedCard=false;
 var firstCard,secondCard;
+var lockBoard=false;
+
+//virar os Cards
+
 function flipCard(){
+    if(lockBoard) return;
+    if(this === firstCard) return;
+
     //this.classList.toggle('flip');
     this.classList.add('flip');
     if(!hasFlippedCard){
@@ -13,13 +20,36 @@ function flipCard(){
     hasFlippedCard=false;
     checkFOrMatch();
 }
+//monitora o clique
 cards.forEach((card) =>{
     card.addEventListener('click', flipCard);
 })
+//checa os matches das cartas
 function checkFOrMatch(){
-    if(firstCard.dataset.card===secondCard.dataset.card){
+    if(firstCard.dataset.card === secondCard.dataset.card){
         disableCards();
         return;
     }
     unflipCards();
+}
+//manter cartas viradas no match
+function disableCards(){
+    firstCard.removeEventListener('click',flipCard);
+    secondCard.removeEventListener('click',flipCard);
+    resetBoard();
+}
+//vira as cartas se não houver match
+function unflipCards(){
+    lockBoard=true;
+    setTimeout(()=>{
+        firstCard.classList.remove('flip');
+        secondCard.classList.remove('flip');
+        //lockBoard=false;
+        resetBoard();
+    },1500);
+}
+
+function resetBoard(){
+    [hasFlippedCard,lockBoard]=[false,false];
+    [firstCard,secondCard]=[null,null];
 }
