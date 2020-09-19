e0d9b9c0583d67d3a5ba1193420f9494fed0affd
elsonss1988 <elsonss1988@gmail.com>
2020-09-19 15:29:24 -0300

    checkMatch

--- js/script.js
+++ js/script.js
@@ -1,8 +1,25 @@
 const cards =document.querySelectorAll(".card");
-
+var hasFlippedCard=false;
+var firstCard,secondCard;
 function flipCard(){
-    this.classList.toggle('flip');
+    //this.classList.toggle('flip');
+    this.classList.add('flip');
+    if(!hasFlippedCard){
+        hasFlippedCard=true
+        firstCard=this;
+        return;
+    }
+    secondCard=this;
+    hasFlippedCard=false;
+    checkFOrMatch();
 }
 cards.forEach((card) =>{
     card.addEventListener('click', flipCard);
 })
+function checkFOrMatch(){
+    if(firstCard.dataset.card===secondCard.dataset.card){
+        disableCards();
+        return;
+    }
+    unflipCards();
+}
